8e63ae69f8471c9fcd8c7442197e399bbd5defeb
Nick Quaranto <nick@quaran.to>
2014-01-29 17:38:20 -0500

    Log output

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,7 @@
 class EntriesController < ApplicationController
   def create
     if params['token'] == Door.config["token"]
-      `
+      Rails.logger.info `
         sudo /usr/local/bin/wemo -f switch "#{Door.config["wemo"]}" on;
         sleep 1;
         sudo /usr/local/bin/wemo -f switch "#{Door.config["wemo"]}" off
